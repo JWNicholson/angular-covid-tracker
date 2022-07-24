9c6366fb99d1e8ad303b72cc43e487ba25f17408
JWNicholson <john.nicholson812@gmail.com>
2022-07-24 10:10:22 -0400

    added dynamic tables to list Counts by top five countries

--- src/app/summary/components/summary-page/summary-page.component.ts
+++ src/app/summary/components/summary-page/summary-page.component.ts
@@ -4,22 +4,16 @@ import { DataService } from 'src/app/shared/services/data.service';
 @Component({
   selector: 'app-summary-page',
   templateUrl: './summary-page.component.html',
-  styleUrls: ['./summary-page.component.css']
+  styleUrls: ['./summary-page.component.css'],
 })
 export class SummaryPageComponent implements OnInit {
+  covidData: any = {};
 
-covidData: any = {};
-
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.dataService.getSummaryData()
-    .subscribe(
-      (data: any) => {
-        this.covidData = data;
-        console.log('Summary Data: ', data)
-      }
-    )
+    this.dataService.getSummaryData().subscribe((data: any) => {
+      this.covidData = data;
+    });
   }
-
 }
